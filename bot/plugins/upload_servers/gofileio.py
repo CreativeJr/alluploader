a0ce8ac45d9db71c85d4ff8543d352313c83ee37
CreativeJr <73512563+CreativeJr@users.noreply.github.com>
2022-07-28 16:10:18 +0700

    Add files via upload

--- bot/plugins/upload_servers/gofileio.py
+++ bot/plugins/upload_servers/gofileio.py
@@ -33,8 +33,7 @@ async def gofileIO(file, client, bot, s_time):
             files = {
                 'file': open(file, 'rb')
             }
-            server = requests.get(url="https://api.gofile.io/getServer").json()["data"]["server"]
-            respose = requests.post('https://{server}.gofile.io/uploadFile', data=files)
+            respose = await session.post('https://store1.gofile.io/uploadFile', data=files)
             dlj = await respose.json()
             dl = dlj['data']['downloadPage']
             await client.edit_message_text(
